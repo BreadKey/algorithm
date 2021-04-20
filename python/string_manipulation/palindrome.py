8f872e819957db34f536d4907ad8727eac102ce7
2breadkey <2breadkey@gmail.com>
2021-04-20 22:18:21 +0900

    solve palindrome

---
+++ python/string_manipulation/palindrome.py
@@ -0,0 +1,6 @@
+import re
+
+def is_palindrome(s: str) -> bool:
+    s = re.sub(r'[^a-z0-9]', '', s.lower())
+
+    return s == s[::-1]
---
+++ python/test_string_manipulation.py
@@ -0,0 +1,4 @@
+from string_manipulation import palindrome
+
+if __name__ == '__main__':
+    assert palindrome.is_palindrome("A man, a plan, a canal: Panama")

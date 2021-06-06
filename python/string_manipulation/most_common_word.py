4458b63b2fda8bb5e43a524a8526eec5fad223c9
2breadkey <2breadkey@gmail.com>
2021-06-06 14:19:55 +0900

    clean answer

--- python/string_manipulation/most_common_word.py
+++ python/string_manipulation/most_common_word.py
@@ -1,3 +1,4 @@
+import collections
 import re
 from typing import Dict, List
 
@@ -6,19 +7,9 @@ def of(paragraph: str, banned: List[str] = []) -> str:
     cleaned: str = re.sub(
         '|'.join(banned + [r'[^a-z ]']), '', paragraph.lower())
 
-    word_map: Dict[str, int] = {}
-
-    result: str = ''
+    word_map = collections.defaultdict(int)
 
     for word in cleaned.split():
-        if word in word_map:
-            word_map[word] += 1
-
-            if word_map[result] < word_map[word]:
-                result = word
-        else:
-            word_map[word] = 1
-            if not result:
-                result = word
-
-    return result
+        word_map[word] += 1
+            
+    return max(word_map, key=word_map.get)

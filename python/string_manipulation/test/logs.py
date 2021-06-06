9ab2cb99d4136e25affda2965a8284375e74c7ac
2breadkey <2breadkey@gmail.com>
2021-06-06 13:31:05 +0900

    refactor test

--- python/string_manipulation/test/logs.py
+++ python/string_manipulation/test/logs.py
@@ -1,5 +1,3 @@
-from string_manipulation import palindrome, logs
-
-assert palindrome.is_palindrome("A man, a plan, a canal: Panama")
+from string_manipulation import logs
 
 assert logs.sort(["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]) == ["let1 art can", "let3 art zero", "let2 own kit dig", "dig1 8 1 5 1", "dig2 3 6"]
---
+++ python/string_manipulation/test/palindrome.py
@@ -0,0 +1,3 @@
+from string_manipulation import palindrome
+
+assert palindrome.is_palindrome("A man, a plan, a canal: Panama")

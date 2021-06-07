fdfd961dc305b5125427f137905ac578187ebe12
2breadkey <2breadkey@gmail.com>
2021-06-07 10:14:45 +0900

    clean answer

--- python/string_manipulation/anagram.py
+++ python/string_manipulation/anagram.py
@@ -1,14 +1,12 @@
 from typing import Dict, List
+import collections
 
 def of(words: List[str]) -> List[List[str]]: 
-    anagram_map: Dict[str, List[str]] = {}
+    anagram_map: Dict[str, List[str]] = collections.defaultdict(list)
     
     for word in words:
         anagram = ''.join(sorted(word))
 
-        if anagram not in anagram_map:
-            anagram_map[anagram] = [word]
-        else:
-            anagram_map[anagram].append(word)
+        anagram_map[anagram].append(word)
 
     return list(anagram_map.values())

14b755b7a31d5d54d3999566a88e35b6e293206d
2breadkey <2breadkey@gmail.com>
2021-06-07 16:06:14 +0900

    change to verb

--- python/linear_data_structure/trapping_rain_water.py
+++ python/linear_data_structure/trapping_rain_water.py
@@ -15,28 +15,28 @@ def calculate(heights: List[int]) -> int:
             
             if not is_descending:
                 if valley[-1] >= valley[0]:
-                    answer += _trap(valley)
+                    answer += _trap_water(valley)
                     valley = valley[-1:]
                     is_descending = True
                 elif valley[-1] < valley[-2]:
-                    answer += _trap(valley[:-1])
+                    answer += _trap_water(valley[:-1])
                     valley = valley[-2:]
                     is_descending = True
 
 
     return answer
 
-def _trap(deque: List[int]) -> int:
-    if len(deque) < 2:
+def _trap_water(valley: List[int]) -> int:
+    if len(valley) < 2:
         return 0
 
-    while deque[0] > deque[-1]:
-        deque = deque[1:]
+    while valley[0] > valley[-1]:
+        valley = valley[1:]
 
     count = 0
-    start_height = deque[0]
+    start_height = valley[0]
 
-    for index in range(1, len(deque) - 1):
-        count += start_height - deque[index]
+    for index in range(1, len(valley) - 1):
+        count += start_height - valley[index]
 
     return count

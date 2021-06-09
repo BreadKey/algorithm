106e39b6d1c9281514f3c4842d77a00708a3096a
2breadkey <2breadkey@gmail.com>
2021-06-09 13:31:42 +0900

    fix solution

--- python/linear_data_structure/test/trapping_rain_water.py
+++ python/linear_data_structure/test/trapping_rain_water.py
@@ -2,3 +2,8 @@ from linear_data_structure import trapping_rain_water
 
 assert(trapping_rain_water.calculate(
     [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) == 6)
+
+assert(trapping_rain_water.calculate([2, 1, 0, 1]) == 1)
+assert(trapping_rain_water.calculate([1, 0, 1, 2]) == 1)
+assert(trapping_rain_water.calculate([3, 2, 1, 0, 1, 2]) == 4)
+assert(trapping_rain_water.calculate([3, 2, 1, 0, 2, 2]) == 3)
--- python/linear_data_structure/trapping_rain_water.py
+++ python/linear_data_structure/trapping_rain_water.py
@@ -12,7 +12,7 @@ def calculate(heights: List[int]) -> int:
             if is_descending:
                 if valley[-1] > valley[-2]:
                     is_descending = False
-            
+
             if not is_descending:
                 if valley[-1] >= valley[0]:
                     answer += _trap_water(valley)
@@ -22,21 +22,21 @@ def calculate(heights: List[int]) -> int:
                     answer += _trap_water(valley[:-1])
                     valley = valley[-2:]
                     is_descending = True
-
+    
+    if len(valley) > 2:
+        answer += _trap_water(valley)
 
     return answer
+
 
 def _trap_water(valley: List[int]) -> int:
     if len(valley) < 2:
         return 0
 
-    while valley[0] > valley[-1]:
-        valley = valley[1:]
-
+    valley_height = min(valley[0], valley[-1])
     count = 0
-    start_height = valley[0]
 
     for index in range(1, len(valley) - 1):
-        count += start_height - valley[index]
+        count += valley_height - valley[index]
 
     return count

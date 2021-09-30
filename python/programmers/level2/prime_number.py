f4feb2bf14e487c58c3cdfbffe4f7546349df95d
2breadkey <2breadkey@gmail.com>
2021-09-30 15:35:00 +0900

    solve 소수 찾기

---
+++ python/programmers/level2/prime_number.py
@@ -0,0 +1,45 @@
+from typing import Dict, List
+
+
+def solution(numbers):
+    return Solution().solve(numbers)
+
+class Solution:
+    answer = 0
+    prime_map: Dict[int, bool] = {}
+
+    def solve(self, numbers: str) -> int:
+        int_numbers = [int(number) for number in numbers]
+
+        self.check(None, int_numbers)
+
+        return self.answer
+
+    def check(self, current: int, remains: List[int]):
+        if current is not None and self.prime_map.get(current) is None:
+            result = self.is_prime(current)
+            self.prime_map[current] = result
+
+            if result:
+                self.answer += 1
+
+        for i in range(len(remains)):
+            copy = remains.copy()
+            next = copy.pop(i)
+
+            self.check(int(f"{current or ''}{next}"), copy.copy())
+            self.check(current, copy.copy())
+
+    # https://en.wikipedia.org/wiki/Primality_test
+    def is_prime(self, n: int) -> bool:
+        """Primality test using 6k+-1 optimization."""
+        if n <= 3:
+            return n > 1
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        i = 5
+        while i ** 2 <= n:
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+            i += 6
+        return True
---
+++ python/programmers/level2/test/prime_number.py
@@ -0,0 +1,4 @@
+from programmers.level2 import prime_number
+
+assert prime_number.solution("17") == 3
+assert prime_number.solution("011") == 2

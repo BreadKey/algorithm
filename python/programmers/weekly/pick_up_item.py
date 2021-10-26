9bfe863bf152b8a6dd3ed1de02b4b64af67c9016
2breadkey <2breadkey@gmail.com>
2021-10-26 18:02:45 +0900

    score 92

--- python/programmers/weekly/pick_up_item.py
+++ python/programmers/weekly/pick_up_item.py
@@ -1,66 +1,215 @@
 from typing import List
 
 
+class Tile:
+    space = 0
+    horizontal = 1
+    vertical = 2
+    bottom_left = 3
+    bottom_right = 4
+    top_left = 5
+    top_right = 6
+    intersection = 7
+    item = 8
+
+
 def solution(rectangles: List[List[int]], character_x: int, character_y: int, item_x: int, item_y: int) -> int:
-    max_x = 0
-    max_y = 0
+    max_x = max(rectangles, key=lambda rectangle: rectangle[2])[2]
+    max_y = max(rectangles, key=lambda rectangle: rectangle[3])[3]
+
+    path = [[Tile.space] * (max_x + 1) for _ in range(max_y + 1)]
 
     for rectangle in rectangles:
-        max_x = max(max_x, rectangle[2])
-        max_y = max(max_y, rectangle[3])
-
-    path = [[0] * (max_x + 1) for _ in range(max_y + 1)]
+        add_rectangle(path, rectangle)
 
     for rectangle in rectangles:
-        for x in range(rectangle[0], rectangle[2] + 1):
-            path[rectangle[1]][x] += 1
-            path[rectangle[3]][x] += 1
-        for y in range(rectangle[1] + 1, rectangle[3]):
-            path[y][rectangle[0]] += 1
-            path[y][rectangle[2]] += 1
+        remove_rectangle(path, rectangle)
 
-    pad = 2
-    stored_path = [[0] * (len(path[0]) + pad) for _ in range(len(path) + pad)]
-    clear_path(path, stored_path)
-    stored_path = remove_pad(stored_path)
+    path[item_y][item_x] = Tile.item
+
+    walk_result: List[int] = []
+
+    up = character_y + 1
+    down = character_y - 1
+    left = character_x - 1
+    right = character_x + 1
+
+    if tile_at(path, character_x, up):
+        walk_result.append(walk(path, character_x, up, 0, 1, 1))
+
+    if tile_at(path, character_x, down):
+        walk_result.append(
+            walk(path, character_x, down,  0, -1, 1))
+
+    if tile_at(path, left, character_y):
+        walk_result.append(
+            walk(path, left, character_y, -1, 0, 1))
+
+    if tile_at(path, right, character_y):
+        walk_result.append(
+            walk(path, right, character_y, 1, 0, 1))
+
+    # print(walk_result)
+
+    return min(walk_result)
 
 
+def add_rectangle(path: List[List[int]], rectangle: List[int]):
+    bottom = rectangle[1]
+    top = rectangle[3]
+
+    left = rectangle[0]
+    right = rectangle[2]
+
+    for x in range(rectangle[0] + 1, rectangle[2]):
+        path[bottom][x] = Tile.intersection if path[bottom][x] else Tile.horizontal
+        if top != bottom:
+            path[top][x] = Tile.intersection if path[top][x] else Tile.horizontal
+
+    for y in range(rectangle[1] + 1, rectangle[3]):
+        path[y][left] = Tile.intersection if path[y][left] else Tile.vertical
+        if right != left:
+            path[y][right] = Tile.intersection if path[y][right] else Tile.vertical
+
+    path[bottom][left] = Tile.intersection if path[bottom][left] else Tile.bottom_left
+    path[bottom][right] = Tile.intersection if path[bottom][right] else Tile.bottom_right
+    path[top][left] = Tile.intersection if path[top][left] else Tile.top_left
+    path[top][right] = Tile.intersection if path[top][right] else Tile.top_right
 
 
-def clear_path(path: List[List[int]], stored_path: List[List[int]], x: int = 0, y: int = 0):
-    if x < 0 or y < 0 or x > len(stored_path[0]) - 1 or y > len(stored_path) - 1:
+def remove_rectangle(path: List[List[int]], rectangle: List[int]):
+    left = rectangle[0]
+    bottom = rectangle[1]
+    right = rectangle[2]
+    top = rectangle[3]
+
+    for x in range(left + 1, right):
+        for y in range(bottom + 1, top):
+            path[y][x] = Tile.space
+
+
+def print_path(path: List[List[int]], character: List[int] = None):
+    reverse = list(reversed(path))
+
+    for i in range(len(reverse)):
+        y = len(reverse) - 1 - i
+        string = f'{y}'
+        for j in range(len(reverse[i])):
+            if character and character[0] == j and character[1] == y:
+                string += '★'
+                continue
+
+            value = reverse[i][j]
+            if value == Tile.space:
+                string += '□'
+            elif value == Tile.horizontal:
+                string += '━'
+            elif value == Tile.vertical:
+                string += '┃'
+            elif value == Tile.bottom_left:
+                string += '┗'
+            elif value == Tile.bottom_right:
+                string += '┛'
+            elif value == Tile.top_left:
+                string += '┏'
+            elif value == Tile.top_right:
+                string += '┓'
+            elif value == Tile.intersection:
+                string += '╋'
+            elif value == Tile.item:
+                string += '⊙'
+        print(string)
+
+
+def walk(path: List[List[int]], character_x: int, character_y: int, step_x: int, step_y: int, step_count: int = 0):
+    tile = tile_at(path, character_x, character_y)
+
+    print(f"Step: {step_count}")
+    print_path(path, [character_x, character_y])
+
+    if step_count == 20:
         return
 
-    if stored_path[y][x] != 0:
-        return
-
-    stored_path[y][x] = -1 if x == 0 or y == 0 or x == len(stored_path[0]) - 1 or y == len(stored_path) - 1 else path[y - 1][x - 1] or -1
-
-    if stored_path[y][x] != -1:
-        return
-
-    clear_path(path, stored_path, x - 1, y)
-    clear_path(path, stored_path, x + 1, y)
-    clear_path(path, stored_path, x, y - 1)
-    clear_path(path, stored_path, x, y + 1)
-    clear_path(path, stored_path, x + 1, y + 1)
-    clear_path(path, stored_path, x - 1, y + 1)
-    clear_path(path, stored_path, x - 1, y - 1)
-    clear_path(path, stored_path, x + 1, y - 1)
-
-def remove_pad(path: List[List[int]]) -> List[List[int]]:
-    path = path[1: len(path) - 1]
-    path = [row[1:len(row) - 1] for row in path]
-
-    return path
-
-def walk(path: List[List[int]], character_x: int, character_y: int, item_x: int, item_y: int, step_x: int, step_y: int, step_count: int = 0):
-    if character_x == item_x and character_y == item_y:
+    if tile == Tile.item:
         return step_count
 
-    if path[character_y + step_y][character_x + step_x] == 1:
-        return walk(path, character_x + step_x, character_y + step_y, item_x, item_y, step_x, step_y, step_count + 1)
-    
-    if step_x == 0:
-        if path[character_y][character_x - 1] == 1:
-            return walk(path, character_x - 1, character_y, item_x, )
+    if tile in [Tile.bottom_left, Tile.top_left]:
+        if step_x == 0:
+            return walk(path, character_x + 1, character_y, 1, 0, step_count + 1)
+        else:
+            next_step_y = 1 if tile == Tile.bottom_left else -1
+            return walk(path, character_x, character_y +
+                        next_step_y, 0, next_step_y, step_count + 1)
+
+    elif tile in [Tile.bottom_right, Tile.top_right]:
+        if step_x == 0:
+            return walk(path, character_x - 1, character_y, -1, 0, step_count + 1)
+        else:
+            next_step_y = 1 if tile == Tile.bottom_right else -1
+            return walk(path, character_x, character_y +
+                        next_step_y, 0, next_step_y, step_count + 1)
+
+    elif tile == Tile.intersection:
+        walk_result = []
+
+        if step_x != 0:
+            down_tile = tile_at(path, character_x, character_y - 1)
+            up_tile = tile_at(path, character_x, character_y + 1)
+
+            if down_tile not in [Tile.space, Tile.intersection, Tile.horizontal]:
+                walk_result.append(
+                    walk(path, character_x, character_y - 1, 0, -1, step_count + 1))
+            if up_tile not in [Tile.space, Tile.intersection, Tile.horizontal]:
+                walk_result.append(
+                    walk(path, character_x, character_y + 1, 0, 1, step_count + 1)
+                )
+
+            if not walk_result:
+                if down_tile not in [Tile.space, Tile.horizontal]:
+                    walk_result.append(
+                        walk(path, character_x, character_y - 1, 0, -1, step_count + 1))
+                if up_tile not in [Tile.space, Tile.horizontal]:
+                    walk_result.append(
+                        walk(path, character_x, character_y +
+                             1, 0, 1, step_count + 1)
+                    )
+
+        else:
+            left_tile = tile_at(path, character_x - 1, character_y)
+            right_tile = tile_at(path, character_x + 1, character_y)
+
+            if left_tile not in [Tile.space, Tile.intersection, Tile.vertical]:
+                walk_result.append(
+                    walk(path, character_x - 1,
+                         character_y, -1, 0, step_count + 1)
+                )
+            if right_tile not in [Tile.space, Tile.intersection, Tile.vertical]:
+                walk_result.append(
+                    walk(path, character_x + 1, character_y, 1, 0, step_count + 1)
+                )
+
+            if not walk_result:
+                if left_tile not in [Tile.space, Tile.vertical]:
+                    walk_result.append(
+                        walk(path, character_x - 1,
+                             character_y, -1, 0, step_count + 1)
+                    )
+                if right_tile not in [Tile.space, Tile.vertical]:
+                    walk_result.append(
+                        walk(path, character_x + 1,
+                             character_y, 1, 0, step_count + 1)
+                    )
+
+        if walk_result:
+            return min(walk_result)
+
+    if tile_at(path, character_x + step_x, character_y + step_y):
+        return walk(path, character_x + step_x, character_y + step_y, step_x, step_y, step_count + 1)
+    return walk(path, character_x - step_x, character_y - step_y, -step_x, -step_y, step_count + 1)
+
+
+def tile_at(path: List[List[int]], x: int, y: int) -> int:
+    if x not in range(len(path[0])) or y not in range(len(path)):
+        return Tile.space
+
+    return path[y][x]
